809166ac6e34a707e4f72bc55ef5fc7861440586
zhangjianzhong <zhangjianzhong>
2025-01-28 13:59:44 +0800

    springboot-mybatis-demo: 完善token校验拦截器逻辑：使用 Interceptor 实现 token 校验功能，校验失败，抛出自定义运行时异常，然后在全局异常捕获处理器中捕获这一类型的异常统一返回业务状态码给前端。

--- springboot-mybatis-demo/src/main/java/com/icu/mybatis/exception/GlobalExceptionHandler.java
+++ springboot-mybatis-demo/src/main/java/com/icu/mybatis/exception/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.icu.mybatis.exception;
 
 import com.icu.mybatis.common.Result;
+import com.icu.mybatis.common.ResultCodeEnum;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -33,5 +36,22 @@ public class GlobalExceptionHandler {
     public Result handleRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
         log.error("全局异常处理器，拦截到异常 [RequestMethodNotSupportedException]", e);
         return Result.build(null, 405, "请求方式不支持");
+    }
+
+    /**
+     * token校验失效异常，捕获到这类异常，提示客户端
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(TokenInvalidException.class)
+    public ResponseEntity<Result> handleTokenInvalidException(TokenInvalidException e) {
+        log.error("全局异常处理器，拦截到异常 [TokenInvalidException]", e);
+        // 构造业务结果对象
+        Result result = Result.build(null, ResultCodeEnum.NOT_PERMISSION);
+
+        // 返回 ResponseEntity，并设置 HTTP 状态码为 401
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(result);
     }
 }
--- springboot-mybatis-demo/src/main/java/com/icu/mybatis/interceptors/TokenInterceptor.java
+++ springboot-mybatis-demo/src/main/java/com/icu/mybatis/interceptors/TokenInterceptor.java
@@ -1,16 +1,21 @@
 package com.icu.mybatis.interceptors;
 
+import com.icu.mybatis.exception.TokenInvalidException;
+import com.icu.mybatis.utils.TokenHelper;
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
-@Service
+@Service // 添加@Service注解，方便依赖注入
 public class TokenInterceptor implements HandlerInterceptor {
+    @Autowired
+    private TokenHelper tokenHelper;
     @Override
     // 目标资源方法执行前执行，返回true：放行，返回false：不放行
     public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
@@ -19,7 +24,20 @@ public class TokenInterceptor implements HandlerInterceptor {
         log.error("TokenInterceptor拦截器执行-[preHandle]：{}", req.getRequestURI());
         log.debug("TokenInterceptor拦截器执行-[preHandle]：{}", req.getRequestURI());
         log.trace("TokenInterceptor拦截器执行-[preHandle]：{}", req.getRequestURI());
-        return true;
+        String token = req.getHeader("token");
+        if(token == null || token.isEmpty()){
+            log.info("token为空");
+            throw new TokenInvalidException("token为空");
+        }
+
+        try {
+            Claims claims = tokenHelper.parseToken(token);
+            log.info("token有效-[{}]： {}", req.getRequestURI(), claims);
+            return true;
+        }catch (Exception ex){
+            log.info("token无效");
+            throw new TokenInvalidException("token无效");
+        }
     }
 
     @Override
