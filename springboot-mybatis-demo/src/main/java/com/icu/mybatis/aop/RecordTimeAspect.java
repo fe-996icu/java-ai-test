dafd35589d0cadb45e1adb8f4015fb162327e291
zhangjianzhong <zhangjianzhong>
2025-01-29 22:27:11 +0800

    springboot-mybatis-demo: Spring AOP 示例，记录方法执行时间

---
+++ springboot-mybatis-demo/src/main/java/com/icu/mybatis/aop/RecordTimeAspect.java
@@ -0,0 +1,27 @@
+package com.icu.mybatis.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect // 标识这是一个切面类 AOP类
+@Component
+public class RecordTimeAspect {
+    // 这个aop方法针对 com.icu.mybatis.services.impl 包下面所有类的所有方法（方法形参也是任意形式的）
+    @Around("execution(* com.icu.mybatis.services.impl.EmployeeServiceImpl.deleteEmployee(..))||" +
+            "execution(* com.icu.mybatis.services.impl.EmployeeServiceImpl.updateEmployee(..))") // 使用逻辑运算符匹配多个连接点
+    public Object recordTime(ProceedingJoinPoint pjp) throws Throwable {
+        // 获取系统当前运行时间
+        long startTime = System.currentTimeMillis();
+
+        // 执行原始方式，并获取返回值
+        Object result = pjp.proceed();
+
+        long endTime = System.currentTimeMillis();
+        log.info("方法[{}] 运行时间：{}ms", pjp.getSignature().toString(), (endTime - startTime));
+        return result;
+    }
+}
