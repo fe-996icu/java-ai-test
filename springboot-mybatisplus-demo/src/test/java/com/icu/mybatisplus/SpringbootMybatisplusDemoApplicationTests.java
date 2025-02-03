9efffef4bdcf90c9bd1d5f55424f023043e4500a
zhangjianzhong <zhangjianzhong>
2025-02-03 14:35:38 +0800

    springboot-mybatisplus-demo: 模块初始化

---
+++ springboot-mybatisplus-demo/src/test/java/com/icu/mybatisplus/SpringbootMybatisplusDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.icu.mybatisplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMybatisplusDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("Hello World");
+    }
+
+}
